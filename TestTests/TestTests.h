169672ef3e25fa1b487ad3f03e07ec871fc8aa98
Harry Ultsch <harry@miesing.fritz.box>
2012-09-23 09:54:09 +0200

    Initial Commit

---
+++ TestTests/TestTests.h
@@ -0,0 +1,13 @@
+//
+//  TestTests.h
+//  TestTests
+//
+//  Created by Harry Ultsch on 23.09.12.
+//  Copyright (c) 2012 Harry Ultsch. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestTests : SenTestCase
+
+@end
---
+++ TestTests/TestTests.m
@@ -0,0 +1,32 @@
+//
+//  TestTests.m
+//  TestTests
+//
+//  Created by Harry Ultsch on 23.09.12.
+//  Copyright (c) 2012 Harry Ultsch. All rights reserved.
+//
+
+#import "TestTests.h"
+
+@implementation TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestTests");
+}
+
+@end
